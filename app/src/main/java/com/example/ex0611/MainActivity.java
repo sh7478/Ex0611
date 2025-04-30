0fdd9a90f4ee5df3fd193b847899b2816ec1a472
sh7478 <sh7478@bs.amalnet.k12.il>
2025-04-30 17:36:28 +0300

    End of design

--- app/src/main/java/com/example/ex0611/MainActivity.java
+++ app/src/main/java/com/example/ex0611/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex0611;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,51 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch autoOrManual;
+    RadioButton rBRed, rBGreen, rBBlue, rBYellow;
+    LinearLayout main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        autoOrManual = findViewById(R.id.autoOrManual);
+        rBRed = findViewById(R.id.rBRed);
+        rBGreen = findViewById(R.id.rBGreen);
+        rBBlue = findViewById(R.id.rBBlue);
+        rBYellow = findViewById(R.id.rBYellow);
+        main = findViewById(R.id.main);
+    }
+
+    public void setBgColor(View view)
+    {
+        if(!autoOrManual.isChecked())
+        {
+            whatColor();
+        }
+    }
+
+    public void manualChange(View view)
+    {
+        whatColor();
+    }
+
+    public void whatColor()
+    {
+        if(rBRed.isChecked())
+        {
+            main.setBackgroundColor(Color.RED);
+        }
+        else if(rBGreen.isChecked())
+        {
+            main.setBackgroundColor(Color.GREEN);
+        }
+        else if(rBBlue.isChecked())
+        {
+            main.setBackgroundColor(Color.BLUE);
+        }
+        else
+        {
+            main.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
